f45c88a709af1ebd0bfbd9ad37038ab72ba3006d
Jonas Gerdes <dev@jonasgerdes.com>
2017-11-26 16:16:53 +0100

    Add way to vote for poll

--- apollo-android/app/src/main/java/de/apps28/apollo/MainActivity.java
+++ apollo-android/app/src/main/java/de/apps28/apollo/MainActivity.java
@@ -1,15 +1,44 @@
 package de.apps28.apollo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import de.apps28.apollo.model.PollRepository;
+import com.google.firebase.auth.FirebaseAuth;
+
+import de.apps28.apollo.create_poll.CreatePollActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SING_IN = 646;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        login();
+    }
+
+
+    private void login() {
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SING_IN) {
+            if (resultCode == RESULT_OK) {
+                start();
+            }
+        }
+    }
+
+    private void start() {
+        if (auth.getCurrentUser() != null) {
+            login();
+        } else {
+            startActivity(new Intent(this, CreatePollActivity.class));
+        }
     }
 }
